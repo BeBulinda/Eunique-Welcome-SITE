8f75144786e227081d3443fa787a4ce3071b8d10
ReConcepts <bellarmine16@gmail.com>
2017-05-14 20:06:45 +0300

    Initial Commit. Migrated the repo to ReConcepts. Eugean, take note.

---
+++ modules/become-mem.php
@@ -0,0 +1,17 @@
+<section class="shop-single-area">
+    <div class="container">
+        <div class="row">
+            
+            <?php
+            require_once("require/become_mem/member.php");
+            ?>  
+            
+                   
+        </div>
+    </div>
+</section>     
+
+                <?php
+                require_once("require/call_for_action.php");
+                ?>
+
